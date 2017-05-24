d68851323f22901b265d65eee1d304124ccfbfc7
Gloria Friesen and Georgina Van Dort <georgina@>
2017-05-24 15:41:34 -0700

    add ResultsActivity to display user inputted word

--- app/src/main/java/com/example/guest/boggle/ResultsActivity.java
+++ app/src/main/java/com/example/guest/boggle/ResultsActivity.java
@@ -1,13 +1,27 @@
 package com.example.guest.boggle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputWordView) TextView mInputWordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String inputWord = intent.getStringExtra("word");
+
+        mInputWordView.setText("This is the user's inputted word: " + inputWord);
     }
 }
