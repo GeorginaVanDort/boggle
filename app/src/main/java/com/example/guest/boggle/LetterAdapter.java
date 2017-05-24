618505172bb5ff2e22eac807034aab2e59fc3839
Gloria Friesen and Georgina Van Dort <georgina@>
2017-05-24 14:21:38 -0700

    add MainActivity and LetterAdapter classes, display 9 letters in grid on MainActivity

---
+++ app/src/main/java/com/example/guest/boggle/LetterAdapter.java
@@ -0,0 +1,54 @@
+package com.example.guest.boggle;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Guest on 5/24/17.
+ */
+
+public class LetterAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mLetters;
+
+    public LetterAdapter(Context context, String[] letters) {
+        this.mContext = context;
+        this.mLetters = letters;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 9;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gridView;
+        if(convertView == null) {
+            gridView = inflater.inflate(R.layout.letter_grid_item, null);
+            TextView letterView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+            letterView.setText(mLetters[position]);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+}
